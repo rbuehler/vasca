c29156f64ca71f83064a2e5dd5a800e4bf9d22dd
Julian Schliwinski <julian.schliwinski@desy.de>
2022-01-13 18:39:06 +0000

    added improved handling of input parameters with corresponding tests

--- uvva/test/test_field.py
+++ uvva/test/test_field.py
@@ -9,10 +9,73 @@ Created on Wed Jan 12 07:32:11 2022
 import sys
 from pprint import pprint
 
+import astropy.units as uu
 import numpy as np
+import pytest
+from astropy.coordinates import SkyCoord
 from loguru import logger
 
 from uvva.field import BaseField, Field, GALEXField
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (
+            {
+                "field_id": None,
+                "field_name": None,
+                "ra": None,
+                "dec": None,
+                "observatory": None,
+                "obsfilter": None,
+            },
+            {
+                "field_id": None,
+                "field_name": None,
+                "ra": None,
+                "dec": None,
+                "observatory": None,
+                "obsfilter": None,
+            },
+        ),
+        (
+            {
+                "field_id": 42,
+                "field_name": "foo",
+                "ra": 23,
+                "dec": 23,
+                "observatory": None,
+                "obsfilter": None,
+            },
+            {
+                "field_id": 1 * uu.dimensionless_unscaled,
+                "field_name": "foo",
+                "ra": 1 * uu.deg,
+                "dec": 1 * uu.deg,
+                "observatory": None,
+                "obsfilter": None,
+            },
+        ),
+    ],
+)
+def test_base_field_input_type(input, expected):
+    par_keys = ["field_id", "field_name", "ra", "dec", "observatory", "obsfilter"]
+    bf = BaseField(**input)
+    assert all([isinstance(bf.__dict__[key], type(expected[key])) for key in par_keys])
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ({"ra": 23, "dec": 23}, SkyCoord(1, 1, unit="deg")),
+        ({"ra": None, "dec": 23 * uu.deg}, None),
+        ({"ra": None, "dec": None}, None),
+    ],
+)
+def test_base_field_center_type(input, expected):
+    bf = BaseField(**input)
+    assert isinstance(bf.center, type(expected))
 
 
 def main():
