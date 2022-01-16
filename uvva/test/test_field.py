46f5fe8850531c5016a6175b856ee2373c116048
Julian Schliwinski <julian.schliwinski@desy.de>
2022-01-16 16:32:40 +0000

    added set attribute test using pytest fixtures

--- uvva/test/test_field.py
+++ uvva/test/test_field.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 12 07:32:11 2022
-
-@author: buehler
-"""
 
 import sys
 from pprint import pprint
@@ -18,64 +13,28 @@ from loguru import logger
 from uvva.field import BaseField, Field, GALEXField
 
 
-@pytest.mark.parametrize(
-    "input, expected",
-    [
-        (
-            {
-                "field_id": None,
-                "field_name": None,
-                "ra": None,
-                "dec": None,
-                "observatory": None,
-                "obsfilter": None,
-            },
-            {
-                "field_id": None,
-                "field_name": None,
-                "ra": None,
-                "dec": None,
-                "observatory": None,
-                "obsfilter": None,
-            },
-        ),
-        (
-            {
-                "field_id": 42,
-                "field_name": "foo",
-                "ra": 23,
-                "dec": 23,
-                "observatory": None,
-                "obsfilter": None,
-            },
-            {
-                "field_id": 1 * uu.dimensionless_unscaled,
-                "field_name": "foo",
-                "ra": 1 * uu.deg,
-                "dec": 1 * uu.deg,
-                "observatory": None,
-                "obsfilter": None,
-            },
-        ),
-    ],
-)
-def test_base_field_input_type(input, expected):
-    par_keys = ["field_id", "field_name", "ra", "dec", "observatory", "obsfilter"]
-    bf = BaseField(**input)
-    assert all([isinstance(bf.__dict__[key], type(expected[key])) for key in par_keys])
+@pytest.fixture
+def new_field():
+    bf = BaseField()
+    bf.tt_field.add_row([42, "name", 2, 3, "obs", "filter"])
+
+    return bf
 
 
-@pytest.mark.parametrize(
-    "input, expected",
-    [
-        ({"ra": 23, "dec": 23}, SkyCoord(1, 1, unit="deg")),
-        ({"ra": None, "dec": 23 * uu.deg}, None),
-        ({"ra": None, "dec": None}, None),
-    ],
-)
-def test_base_field_center_type(input, expected):
-    bf = BaseField(**input)
-    assert isinstance(bf.center, type(expected))
+def test_set_field_attr_type(new_field):
+    new_field.set_field_attr()
+    expected = {
+        "id": 1 * uu.dimensionless_unscaled,
+        "name": "name",
+        "ra": 1 * uu.deg,
+        "dec": 1 * uu.deg,
+        "observatory": "obs",
+        "obsfilter": "filter",
+        "center": SkyCoord(1, 1, unit="deg"),
+    }
+    assert all(
+        [isinstance(new_field.__dict__[key], type(expected[key])) for key in expected]
+    )
 
 
 def test_base_field_print_info():
@@ -96,7 +55,7 @@ def main():
     # logging
     logger.enable("uvva")
 
-    # Tests to excecute
+    # Tests to execute
     # test_base_field_print_info()
     test_base_field_io()
 
