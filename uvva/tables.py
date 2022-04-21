77ae2730b98db59b8c4989451180dd5067b4e738
Rolf Buehler <rolf.buehler@desy.de>
2022-04-21 11:49:24 +0200

    Merge branch 'main' of git@gitlab.desy.de:ultrasat-camera/uc_uvva.git

---
+++ uvva/tables.py
@@ -0,0 +1,561 @@
+import os
+from astropy import units as uu
+from astropy.table import Table
+from loguru import logger
+import h5py
+from astropy.io import fits
+from astropy.wcs import wcs
+
+
+dimless = uu.dimensionless_unscaled
+
+# global paths
+# path to the dir. of this file
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = FILE_DIR + "/../"  # path to the root directory of the repository
+
+# global dictionaries defining the table structures
+base_field = {
+    "tt_field": {
+        "names": ["field_id", "name", "ra", "dec", "observatory", "obsfilter"],
+        "dtype": ["uint64", "S64", "float64", "float64", "S64", "S64"],
+        "units": ["1", "", "degree", "degree", "", ""],
+        "descriptions": [
+            "Field ID nr.",
+            "Field name",
+            "Center RA of the field (J2000)",
+            "Center Dec of the field (J2000)",
+            "Telescope of the observation (e.g. GALEX)",
+            "Filter of the observation (e.g. NUV)",
+        ],
+        "meta": {"DATAPATH": "None", "INFO": "Field information table"},
+    },
+    "tt_visits": {
+        "names": ["vis_id", "time_bin_start", "time_bin_size"],
+        "dtype": ["uint64", "float64", "float64"],
+        "units": ["1", "d", "s"],
+        "descriptions": [
+            "Visit ID nr.",
+            "Visit exposure start date and time in MJD",
+            "Visit exposure time in s",
+        ],
+        "meta": {"INFO": "Visit information table"},
+    },
+    "tt_detections": {
+        "names": [
+            "vis_id",
+            "src_id",
+            "det_id",
+            "ra",
+            "dec",
+            "pos_err",
+            "mag",
+            "mag_err",
+        ],
+        "dtype": [
+            "uint64",
+            "uint64",
+            "uint64",
+            "float64",
+            "float64",
+            "float64",
+            "float32",
+            "float32",
+        ],
+        "units": [
+            "1",
+            "1",
+            "1",
+            "degree",
+            "degree",
+            "degree",
+            "1",
+            "1",
+        ],
+        "descriptions": [
+            "Visit ID associated to the visit detection",
+            "Source ID associated to the visit detection",
+            "Visit detection ID",
+            "Visit detection RA (J2000)",
+            "Visit detection Dec (J2000)",
+            "Visit position error",
+            "Visit detection magnitude",
+            "Visit detection magnitude error",
+        ],
+        "meta": {
+            "INFO": "Visit detections table",
+        },
+    },
+    "tt_ref_sources": {
+        "names": [
+            "det_id",
+            "ra",
+            "dec",
+            "pos_err",
+            "mag",
+            "mag_err",
+        ],
+        "dtype": [
+            "uint64",
+            "float64",
+            "float64",
+            "float32",
+            "float32",
+            "float32",
+        ],
+        "units": [
+            "1",
+            "degree",
+            "degree",
+            "degree",
+            "1",
+            "1",
+        ],
+        "descriptions": [
+            "Reference source ID nr.",
+            "Reference source RA (J2000)",
+            "Reference source Dec (J2000)",
+            "Reference position error",
+            "Reference source magnitude",
+            "Reference source magnitude error",
+        ],
+        "meta": {
+            "INFO": "Reference detections table",
+        },
+    },
+    "tt_sources": {
+        "names": ["src_id", "ra", "dec", "nr_det", "flag"],
+        "dtype": ["uint32", "float32", "float32", "uint32", "int32"],
+        "units": ["1", "degree", "degree", "1", "1"],
+        "descriptions": [
+            "Source ID nr.",
+            "Source RA (J2000)",
+            "Source Dec (J2000)",
+            "Number of visit detections of the source",
+            "Source flags",
+        ],
+        "meta": {"INFO": "Source infomation table", "CLUSTALG": "None"},
+    },
+    "tt_sources_lc": {
+        "names": ["time_start", "time_delta"],
+        "dtype": ["float64", "float64"],
+        "units": ["d", "s"],
+        "descriptions": [
+            "Visit exposure start date and time in MJD",
+            "Visit exposure stop date and time in MJD",
+        ],
+        "meta": {"INFO": "Light curve magnitude flux and error table"},
+    },
+}
+galex_field = {
+    "tt_visits": {
+        "names": [
+            *base_field["tt_visits"]["names"],
+            "time_bin_size_alt_filt",
+            "ra",
+            "dec",
+        ],
+        "dtype": [
+            *base_field["tt_visits"]["dtype"],
+            "float64",
+            "float64",
+            "float64",
+        ],
+        "units": [
+            *base_field["tt_visits"]["units"],
+            "s",
+            "degree",
+            "degree",
+        ],
+        "descriptions": [
+            *base_field["tt_visits"]["descriptions"],
+            "Visit exposure time of the alternative filter in s",
+            "Center RA of the visit FoV (J2000)",
+            "Center Dec of the visit FoV (J2000)",
+        ],
+        "meta": {
+            **base_field["tt_visits"]["meta"],
+        },
+    },
+    "tt_detections": {
+        "names": [
+            *base_field["tt_detections"]["names"],
+            "r_fov",
+            "artifacts",
+            "point_src_prob",
+            "bright_match",
+            "flux_f60",
+            "flux_f60_err",
+            "flux_f38",
+            "flux_f38_err",
+            "E_bv",
+        ],
+        "dtype": [
+            *base_field["tt_detections"]["dtype"],
+            "float32",
+            "int32",
+            "float32",
+            "int32",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+        ],
+        "units": [
+            *base_field["tt_detections"]["units"],
+            "degree",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+        ],
+        "descriptions": [
+            *base_field["tt_detections"]["descriptions"],
+            "Distance from center of FOV in degrees",
+            "Logical OR of artifact flags",
+            "Point-source probability: 0.0 (resolved), 1.0 (unresolved)",
+            "Detection matched to a known star (bright_match=1)",
+            "Flux in a fixed circular 6.0 arcsec radius aperture in cts/sec",
+            "Flux error in a fixed circular 6.0 arcsec radius aperture in cts/sec",
+            "Flux in a fixed circular 3.8 arcsec radius aperture in cts/sec",
+            "Flux error in a fixed circular 3.8 arcsec radius aperture in cts/sec",
+            "Galactic reddening expressed as E(B-V) [mag]",
+        ],
+        "meta": {
+            **base_field["tt_detections"]["meta"],
+            "PRECUTS": "List of pre-cuts",
+        },
+    },
+    "tt_ref_sources": {
+        "names": [
+            *base_field["tt_ref_sources"]["names"],
+            "r_fov",
+            "artifacts",
+            "point_src_prob",
+            "bright_match",
+            "flux_f60",
+            "flux_f60_err",
+            "flux_f38",
+            "flux_f38_err",
+            "E_bv",
+        ],
+        "dtype": [
+            *base_field["tt_ref_sources"]["dtype"],
+            "float32",
+            "int32",
+            "float32",
+            "int32",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+        ],
+        "units": [
+            *base_field["tt_ref_sources"]["units"],
+            "degree",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+        ],
+        "descriptions": [
+            *base_field["tt_ref_sources"]["descriptions"],
+            "Distance from center of FOV in degrees",
+            "Logical OR of artifact flags",
+            "Point-source probability: 0.0 (resolved), 1.0 (unresolved)",
+            "Detection matched to a known star (bright_match=1)",
+            "Flux in a fixed circular 6.0 arcsec radius aperture in cts/sec",
+            "Flux error in a fixed circular 6.0 arcsec radius aperture in cts/sec",
+            "Flux in a fixed circular 3.8 arcsec radius aperture in cts/sec",
+            "Flux error in a fixed circular 3.8 arcsec radius aperture in cts/sec",
+            "Galactic reddening expressed as E(B-V) [mag]",
+        ],
+        "meta": {
+            **base_field["tt_ref_sources"]["meta"],
+            "PRECUTS": "List of pre-cuts",
+        },
+    },
+}
+region = {
+    "tt_fields": {
+        "names": [
+            *base_field["tt_field"]["names"],
+            "size",
+        ],
+        "dtype": [
+            *base_field["tt_field"]["dtype"],
+            "float64",
+        ],
+        "units": [
+            *base_field["tt_field"]["units"],
+            "degree",
+        ],
+        "descriptions": [
+            *base_field["tt_field"]["descriptions"],
+            "Field radius or box size (depending on the observatory)"
+
+        ],
+        "meta": {
+            **base_field["tt_visits"]["meta"],
+        },
+    },
+}
+
+# global, combined dictionary
+class_keys = ["base_field", "galex_field", "region"]
+class_dicts = [base_field, galex_field, region]
+dd_uvva_tables = {c_key: c_dict for c_key,
+                  c_dict in zip(class_keys, class_dicts)}
+
+
+class TableCollection(object):
+    def __init__(self):
+        # Configure logger
+        # adds the class name as an extra key; accessible vie the handler format
+        logger.configure(extra={"classname": self.__class__.__name__})
+
+        self.combined_templates = dd_uvva_tables
+
+        self._table_names = list()
+
+    @staticmethod
+    def table_from_template(data, template_name):
+        """
+        Creates a new astropy table.
+
+        Parameters
+        ----------
+        data : list, array-like
+            Data of the table with shape (n, n_cols) or as dictionaty with the
+            key corresponding to the templates columns.
+        template_name : str
+            Identifier to select a table template. Templates are selected by
+            setting the class key and a corresponding table key in one string
+            separated by a colon, e.g. template_name=<class_key>:<table_key>.
+
+        Returns
+        -------
+        astropy.table.Table
+        """
+
+        # Takes pre-defined template dictionary
+        templates = dd_uvva_tables
+
+        # Parse template identifier keys
+        if template_name is not None:
+            class_key = template_name.split(":")[0]
+            table_key = template_name.split(":")[1]
+
+        # Generate lists of available class and table keys
+        class_keys = [clss for clss in templates.keys()]
+        table_keys = [
+            tbl for clss in templates.keys() for tbl in templates[clss].keys()
+        ]
+
+        # Check if template exists for template_name
+        if class_key not in class_keys:
+            raise KeyError(
+                f"Unknown class key '{class_key}'. Choose one from {class_keys}"
+            )
+        if table_key not in table_keys:
+            raise KeyError(
+                f"Unknown table key '{table_key}'. Choose one from {table_keys}"
+            )
+
+        # Create table
+        tt_out = Table(data=data, **templates[class_key][table_key])
+
+        # logging
+        logger.debug(f"Created new table from template '{template_name}'.")
+        return tt_out
+
+    def add_table(self, data, template_name):
+        """
+        Add a UVVA table to the field.
+
+        Parameters
+        ----------
+        data : list, array-like
+            Data of the table with shape (n, n_cols) or as dictionary with the
+            key corresponding to the templates columns.
+        template_name : str
+            Identifier to select a table template. Templates are selected by
+            setting the class key and a corresponding table key in one string
+            separated by a colon, e.g. template_name=<class_key>:<table_key>.
+
+        """
+        logger.info(f"Adding table '{template_name}'")
+
+        table_key = template_name.split(":")[1]
+
+        if table_key in self._table_names:
+            logger.warning(f"Table '{table_key}' already exists, overwriting")
+        else:
+            self._table_names.append(table_key)
+
+        setattr(self, table_key, self.table_from_template(
+            data, template_name))
+
+    def write_to_fits(self, file_name="field_default.fits", overwrite=True):
+        """
+        Write tables and image of a field to a fits file.
+
+        Parameters
+        ----------
+        file_name : str, optional
+            File name. The default is "field_default.fits".
+        overwrite : bool, optional
+            Overwrite existing file. The default is True.
+
+        Returns
+        -------
+        None.
+
+        """
+        logger.info(f"Writing file with name '{file_name}'")
+
+        # Create HDU list and write
+        hdup = fits.PrimaryHDU()
+
+        # Check if image data is set and add to primary HDU
+        if hasattr(self, 'ref_img'):
+            if self.ref_img is not None and self.ref_wcs is not None:
+                logger.debug("Storing image data'")
+                hdup = fits.PrimaryHDU(
+                    self.ref_img, header=self.ref_wcs.to_header())
+
+        hdus = [hdup]
+        new_hdul = fits.HDUList([hdup])
+        new_hdul.writeto(file_name, overwrite=overwrite)
+
+        for key in self._table_names:
+            if key in self.__dict__:
+                logger.debug(f"Writing table '{key}'")
+                self.__dict__[key].write(file_name, append=True)
+
+        # Rename extensions to table names
+        ext_nr = 0
+        with fits.open(file_name, "update") as ff:
+            for key in self._table_names:
+                if key in self.__dict__:
+                    ext_nr += 1
+                    ff[ext_nr].header["EXTNAME"] = key
+
+    def load_from_fits(self, file_name="field_default.fits"):
+        """
+        Loads field from a fits file
+
+        Parameters
+        ----------
+        file_name : str, optional
+            File name. The default is "field_default.fits".
+
+        Returns
+        -------
+        None.
+
+        """
+        logger.info(f"Loading file with name '{file_name}'")
+        with fits.open(file_name) as ff:
+            # Load tables
+            # get available table names
+            table_names = [
+                hdu.header["EXTNAME"]
+                for hdu in ff[1:]
+                if hdu.header["EXTNAME"].startswith("tt_")
+            ]
+            # loop over tables
+            for name in table_names:
+                logger.debug(f"Loading table '{name}'")
+                # add to table manifest
+                if name in self._table_names:
+                    logger.warning(
+                        f"Table '{name}' already exists, overwriting.")
+                else:
+                    self._table_names.append(name)
+                setattr(self, name, Table.read(file_name, hdu=name))
+
+            # Load image data
+            if hasattr(self, 'ref_img'):
+                self.ref_img = ff[0].data
+                if self.ref_img is not None:
+                    self.ref_wcs = wcs.WCS(ff[0].header)
+                else:
+                    self.ref_wcs = None
+
+    def write_to_hdf5(self, file_name="field_default.hdf5"):
+        """
+        Write tables of a field to a hdf5 file.
+
+        Parameters
+        ----------
+        file_name : str, optional
+            File name. The default is "field_default.fits".
+        overwrite : bool, optional
+            Overwrite existing file. The default is True.
+
+        Returns
+        -------
+        None.
+
+        """
+
+        logger.info(f"Writing file with name '{file_name}'")
+
+        ii = 0
+        for key in self._table_names:
+            if key in self.__dict__:
+                logger.debug(f"Writing table '{key}'")
+                ii += 1
+                if ii == 1:
+                    self.__dict__[key].write(
+                        file_name,
+                        path="TABDATA/" + key,
+                        overwrite=True,
+                        serialize_meta=True,
+                    )
+                else:
+                    self.__dict__[key].write(
+                        file_name,
+                        path="TABDATA/" + key,
+                        append=True,
+                        serialize_meta=True,
+                    )
+
+    def load_from_hdf5(self, file_name="field_default.hdf5"):
+        """
+        Loads field from a hdf5 file
+
+        Parameters
+        ----------
+        file_name : str, optional
+            File name. The default is "field_default.hdf5".
+
+        Returns
+        -------
+        None.
+
+        """
+
+        logger.info(f"Loading file with name '{file_name}'")
+        in_file = h5py.File(file_name, "r")
+
+        for table in in_file["TABDATA"].keys():
+            if "meta" in str(table):
+                continue
+            logger.debug(f"Loading table '{table}'")
+            self._table_names.append(str(table))
+            setattr(
+                self, str(table), Table.read(
+                    file_name, path="TABDATA/" + str(table))
+            )
