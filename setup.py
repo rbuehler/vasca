525a5dcd87c7deddfff0106da20e8876265c95ff
Rolf Buehler <rolf.buehler@desy.de>
2022-10-19 16:24:09 +0200

    Renaming

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import find_packages, setup
 
 setup(
     packages=find_packages(),
-    entry_points={"console_scripts": ["uvva_pipe=uvva.uvva_pipe:run_from_file"]},
+    entry_points={"console_scripts": ["vasca_pipe=vasca.vasca_pipe:run_from_file"]},
 )
