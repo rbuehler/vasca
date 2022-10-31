6f6fa151096b4add273a1060c778bf25ed4639d2
Rolf Buehler <rolf.buehler@desy.de>
2022-10-31 11:29:00 +0100

    Plotting into one module

--- vasca/vasca_pipe.py
+++ vasca/vasca_pipe.py
@@ -8,7 +8,6 @@ Script that runs the VASCA pipeline.
 import argparse
 import os
 import sys
-from collections import OrderedDict
 from itertools import zip_longest
 
 # from multiprocessing import Pool
@@ -22,6 +21,7 @@ import yaml
 from loguru import logger
 
 from vasca.region import Region
+import vasca.visualization as vvis
 
 
 def set_config(cfg_file):
@@ -49,103 +49,6 @@ def set_config(cfg_file):
     vasca_cfg["cfg_file"] = cfg_file
 
     return vasca_cfg
-
-
-# TODO: there are some GALEX specific variables
-# will need to be adapted to other missions
-def diagnostic(tc, table_name, plot_type):
-
-    det_vars = OrderedDict()
-    if plot_type == "hist":
-        # Detections diagnostic
-        if table_name == "tt_detections":
-            det_vars["s2n"] = {"logx": True}
-            det_vars["mag"] = {"range": [14.5, 26.5]}
-            det_vars["mag_err"] = {"range": [0.0, 3.0]}
-            det_vars["r_fov"] = {"range": [0.0, 0.7]}
-            det_vars["point_src_prob"] = {}
-            det_vars["artifacts"] = {"histtype": "step"}
-            fig, axs = plt.subplots(3, 2, figsize=(18, 12), squeeze=False)
-        elif table_name == "tt_sources":
-            det_vars["nr_det"] = {}
-            det_vars["nr_uls"] = {}
-            det_vars["mag_mean"] = {}
-            det_vars["mag_rchiq"] = {"logx": True, "range": [-3, 3]}
-            det_vars["mag_dmax"] = {}
-            det_vars["mag_dmax_sig"] = {"logx": True, "range": [-3, 2]}
-            det_vars["mag_var"] = {"logx": True, "range": [-3, 0]}
-            det_vars["ul_weight"] = {"bins": 100}
-            fig, axs = plt.subplots(2, 4, figsize=(22, 12), squeeze=False)
-        else:
-            logger.warning("Diegnostic for table '{table_name}' not defined")
-
-    elif plot_type == "scatter":
-        # Detections diagnostic
-        if table_name == "tt_detections":
-            det_vars[("s2n", "mag")] = {
-                "invert_yaxis": True,
-                "xlim": [1, 100],
-                "ylim": [14.5, 26.5],
-            }
-            det_vars[("r_fov", "mag")] = {"invert_yaxis": True, "ylim": [15.5, 26.5]}
-            det_vars[("point_src_prob", "mag")] = {
-                "invert_yaxis": True,
-                "ylim": [14.5, 26.5],
-            }
-            det_vars[("artifacts", "mag")] = {
-                "invert_yaxis": True,
-                "ylim": [14.5, 26.5],
-            }
-            det_vars[("r_fov", "artifacts")] = {}
-            det_vars[("mag_err", "mag")] = {
-                "xlim": [0.01, 3],
-                "invert_yaxis": True,
-                "ylim": [14.5, 26.5],
-            }
-            fig, axs = plt.subplots(3, 2, figsize=(14, 12), squeeze=False)
-        elif table_name == "tt_sources":
-            det_vars[("mag_rchiq", "mag_dmax_sig")] = {"xscale": "log"}
-            det_vars[("mag_rchiq", "ul_weight")] = {"xscale": "log"}
-            det_vars[("mag_dmax_sig", "ul_weight")] = {}
-            det_vars[("mag_dmax_sig", "mag_mean")] = {
-                "invert_yaxis": True,
-                "ylim": [17.5, 24.5],
-            }
-            det_vars[("ul_weight", "mag_mean")] = {
-                "invert_yaxis": True,
-                "ylim": [17.5, 24.5],
-            }
-            det_vars[("mag_rchiq", "mag_mean")] = {
-                "xscale": "log",
-                "invert_yaxis": True,
-                "ylim": [17.5, 24.5],
-            }
-            det_vars[("mag_var", "mag_mean")] = {
-                "invert_yaxis": True,
-                "ylim": [17.5, 24.5],
-            }
-            det_vars[("nr_uls", "mag_mean")] = {
-                "invert_yaxis": True,
-                "ylim": [17.5, 24.5],
-            }
-            fig, axs = plt.subplots(2, 4, figsize=(22, 12), squeeze=False)
-        else:
-            logger.warning("Diegnostic for table '{table_name}' not defined")
-    else:
-        logger.warning("Plot type '{plot_type}' unknown")
-
-    axs = axs.flatten()
-    ax_ctr = 0
-    for var, plot_arg in det_vars.items():
-        if plot_type == "hist":
-            tc.plot_hist(table_name, var, axs[ax_ctr], **plot_arg)
-        elif plot_type == "scatter":
-            tc.plot_scatter(table_name, var[0], var[1], axs[ax_ctr], **plot_arg)
-        ax_ctr += 1
-
-    plt.tight_layout()
-    plt.legend()
-    return fig
 
 
 def set_logger(vasca_cfg):
@@ -245,7 +148,7 @@ def run_field(field, vasca_cfg):
     field.write_to_fits(field_dir + "field_" + str(field.field_id) + ".fits")
 
     # Plot sky maps
-    fig_sky = field.plot_sky(plot_detections=True)
+    fig_sky = vvis.plot_field_sky(field, plot_detections=True)
     if vasca_cfg["general"]["hd_img_out"]:
         fig_sky.savefig(
             field_dir + "sky_map_hr_" + str(field.field_id) + ".png",
@@ -266,7 +169,7 @@ def run_field(field, vasca_cfg):
         ("sources", "scatter"),
     ]
     for diag in diags:
-        fig_diag = diagnostic(field, "tt_" + diag[0], diag[1])
+        fig_diag = vvis.plot_pipe_diagnostic(field, "tt_" + diag[0], diag[1])
         fig_diag.savefig(
             field_dir + diag[1] + "_" + diag[0] + "_" + str(field.field_id) + ".png",
             dpi=150,
@@ -284,7 +187,9 @@ def run_field(field, vasca_cfg):
         ):
             fig_lc = plt.figure(figsize=(10, 10))
             fd_src_ids_chunk = np.array(fd_src_ids_chunk, dtype=np.int64).flatten()
-            fd_src_ids_chunk = np.delete(fd_src_ids_chunk, np.where(fd_src_ids_chunk == -1))
+            fd_src_ids_chunk = np.delete(
+                fd_src_ids_chunk, np.where(fd_src_ids_chunk == -1)
+            )
             field.plot_light_curve(fd_src_ids_chunk, ylim=[24.5, 15.5])
             plt.tight_layout()
             srcs_name = "_".join([str(elem) for elem in fd_src_ids_chunk])
@@ -383,7 +288,7 @@ def run(vasca_cfg):
     )
 
     # Make diagnostics
-    fig_diag_rgsrcs_hist = diagnostic(rg, "tt_sources", "hist")
+    fig_diag_rgsrcs_hist = vvis.plot_pipe_diagnostic(rg, "tt_sources", "hist")
     fig_diag_rgsrcs_hist.savefig(
         region_dir
         + "/region_diagnostic_srcs_hist_"
@@ -393,7 +298,7 @@ def run(vasca_cfg):
     )
     plt.close(fig_diag_rgsrcs_hist)
 
-    fig_diag_rgsrcs_scat = diagnostic(rg, "tt_sources", "scatter")
+    fig_diag_rgsrcs_scat = vvis.plot_pipe_diagnostic(rg, "tt_sources", "scatter")
     fig_diag_rgsrcs_scat.savefig(
         region_dir
         + "/region_diagnostic_srcs_scat_"
@@ -411,7 +316,15 @@ def run(vasca_cfg):
         yaml.dump(vasca_cfg, yaml_file)
 
 
-def run_from_file(file_name=os.getcwd() + "/vasca_cfg.yaml"):
+def run_from_file():
+    """
+    Run VASCA pipeline from a configuration file passed with the ArgumentParser
+
+    Returns
+    -------
+    None.
+
+    """
 
     # Argument parsing
     parser = argparse.ArgumentParser()
@@ -423,7 +336,7 @@ def run_from_file(file_name=os.getcwd() + "/vasca_cfg.yaml"):
         + f"\n Running vasca_pipe.py with configuration file:\n {args.cfg}\n"
         + 50 * "-"
     )
-    set_config(args.cfg)
+    vasca_cfg = set_config(args.cfg)
     run(vasca_cfg)
 
 
