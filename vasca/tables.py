81c543eac7f9d81a4d0466bedeee3a14f7a9ab8b
Rolf Buehler <rolf.buehler@desy.de>
2023-03-30 16:11:34 +0200

    Added field regions
    Former-commit-id: a621c0e00d69701bc7744892da45b98b671e3906

--- vasca/tables.py
+++ vasca/tables.py
@@ -455,7 +455,7 @@ class TableCollection(object):
             if "range" in selections.keys():
                 for var, vals in selections["range"].items():
                     sel = sel * (tt[var] >= vals[0]) * (tt[var] <= vals[1])
-                    logger.debug(
+                    logger.info(
                         f"AND selecting '{var}' {vals}, "
                         f"kept: {100*sel.sum()/nr_sel : .4f}%"
                     )
@@ -466,7 +466,7 @@ class TableCollection(object):
                     no_art = sel * (tt[var].data.astype("int") == 0)
                     bit = bitmask.bitfield_to_boolean_mask(tt[var], ignore_flags=vals)
                     sel = sel * (no_art + bit)
-                    logger.debug(
+                    logger.info(
                         f"AND selecting bitmask '{var}' keep {vals}, "
                         f"kept: {100*sel.sum()/nr_sel : .4f}%"
                     )
@@ -476,7 +476,7 @@ class TableCollection(object):
             if "range" in selections.keys():
                 for var, vals in selections["range"].items():
                     sel_or = sel_or + (tt[var] >= vals[0]) * (tt[var] <= vals[1])
-                    logger.debug(
+                    logger.info(
                         f"OR selecting '{var}' {vals}, "
                         f"kept: {100*sel_or.sum()/nr_sel : .4f}%"
                     )
