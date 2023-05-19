7a574809bbb899f356dbcc051f83b7401d3978c3
Julian Schliwinski <julian.schliwinski@desy.de>
2023-05-19 15:58:00 +0200

    ♻️FIX: Fixed a problem related to file handling
    Former-commit-id: 81e06cafe056b170099bdcba76064d4ffac6a6c7

--- vasca/examples/xmatch_scan.py
+++ vasca/examples/xmatch_scan.py
@@ -22,24 +22,30 @@ acms_cat_list = list(get_acms_cat_info().keys())
 
 for catalog in tqdm(acms_cat_list, total=len(acms_cat_list), desc="ACMS Catalogs"):
     try:
-        api = "ampel"
-        radius = 15.0
-        coords = vasca_cat["ra", "dec"]
-        ids = vasca_cat["rg_src_id"]
-
-        tt_matched = xmatch(
-            api=api,
-            catalog=catalog,
-            radius=radius,
-            coords=coords,
-            ids=ids,
-            dropna=False,
-            hide_progress=False,
-        )
-
         out_file = f"{out}/tt_matched_{catalog}.csv"
+
         if not os.path.isfile(out_file):
+            api = "ampel"
+            radius = 15.0
+            coords = vasca_cat["ra", "dec"]
+            ids = vasca_cat["rg_src_id"]
+
+            tt_matched = xmatch(
+                api=api,
+                catalog=catalog,
+                radius=radius,
+                coords=coords,
+                ids=ids,
+                dropna=False,
+                hide_progress=False,
+            )
+
             logger.info(f"Writing query results to '{out_file}'")
             tt_matched.write(f"{out}", format="csv", overwrite=True)
+        else:
+            logger.info(
+                f"Skipping x-matching for catalog '{catalog}'. "
+                f"File already exists at '{out_file}'"
+            )
     except Exception as e:
         logger.exception(f"Failed to query catalog '{catalog}'. Exception:\n{e}")
