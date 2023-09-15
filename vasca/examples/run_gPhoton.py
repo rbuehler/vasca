5f5fee650649527e06e6f7d6cb57a6cbe438f23d
Rolf Buehler <rolf.buehler@desy.de>
2023-09-15 08:18:43 +0200

    Modified to new file naming
    Former-commit-id: 27ed29506b6ea920cc631e9ac9f95c1051f62d1c

--- vasca/examples/run_gPhoton.py
+++ vasca/examples/run_gPhoton.py
@@ -3,57 +3,82 @@
 
 import gPhoton
 import numpy as np
-from astropy.io import ascii
 import os
 from astropy import units as uu
+from vasca.region import Region
+from astropy.table import Table
 
-src_str = """label	ra	dec
-GALEX_J221409.8+005245 333.5413675869932 0.8794347888579439
-PB_5130 334.61900740472345 -0.0033813554025151345
-GALEX_J100236.8+023834 150.65361426058803 2.643111670533707
-GALEX_J161634.4+541011 244.14364033210146 54.16991388586225
-"""
-
-tt_srcs = ascii.read(src_str)
-print(tt_srcs)
+region_name = "TDS"  # "WD" #"MDIS_10-800" # _ELAISN1
+srcs_ids = [
+    1891,
+    3403,
+    16149,
+    45461,
+]
 outdir = "./resources/gPhoton_out/"
+region_fname = (
+    "./vasca_pipeline/" + region_name + "/region_" + region_name + "_cat.fits"
+)
 
 ron = (6 * uu.arcsec).to(uu.deg).value  # Radius of signal annulus
 roff1 = (10 * uu.arcsec).to(uu.deg).value  # Inner radius of background ring
 roff2 = (15 * uu.arcsec).to(uu.deg).value  # Outer radius of background ring
+bands = ["FUV"]  # "NUV",
 
-for run_src_idx in range(len(tt_srcs)):
-    pos_ra = tt_srcs["ra"][run_src_idx]
-    pos_dec = tt_srcs["dec"][run_src_idx]
 
-    outfile_fin = outdir + "gPhoton_" + str(tt_srcs["label"][run_src_idx]) + "_fin.npy"
-    outfile_app = outdir + "gPhoton_" + str(tt_srcs["label"][run_src_idx]) + "_app.npy"
+rg = Region()
+rg.load_from_fits(region_fname)
 
-    # Run or load gFind
-    if os.path.isfile(outfile_fin):
-        dd_gfind = np.load(outfile_fin, allow_pickle="TRUE").item()
-        t_bins = list(zip(dd_gfind["NUV"]["t0"], dd_gfind["NUV"]["t1"]))
-        print("Number of time bins:", len(t_bins))
-    else:
-        print("Running query with gFind for:\n", tt_srcs[run_src_idx])
-        dd_gfind = gPhoton.gFind(
-            band="NUV", skypos=[pos_ra, pos_dec]
-        )  # ,maxgap=100.,minexp=100.
-        t_bins = list(zip(dd_gfind["NUV"]["t0"], dd_gfind["NUV"]["t1"]))
-        np.save(outfile_fin, dd_gfind)
+# Subselect sources based on choice
+if len(srcs_ids) > 0:
+    rg.tt_sources.add_index("rg_src_id")
+    idx_srcs = rg.tt_sources.loc_indices["rg_src_id", srcs_ids]
+    tt_srcs = Table(rg.tt_sources[idx_srcs])
+else:
+    tt_srcs = rg.tt_sources
 
-    # Run or load gApperture
-    if os.path.isfile(outfile_app):
-        dd_gph = np.load(outfile_app, allow_pickle="TRUE").item()
-    else:
-        print("Running lightcurve with gAperture..")
-        dd_gaperture = gPhoton.gAperture(
-            band="NUV",
-            skypos=[pos_ra, pos_dec],
-            radius=ron,
-            annulus=[roff1, roff2],
-            tranges=t_bins,
+
+for band in bands:
+    print("--- Running for band", band)
+    for rg_src_id in srcs_ids:
+
+        tt_src = tt_srcs[tt_srcs["rg_src_id"] == rg_src_id]
+
+        # Load file
+        fname_base = (
+            "gPhoton_ra"
+            + str(round(tt_src["ra"][0], 5))
+            + "_dec"
+            + str(round(tt_src["dec"][0], 5))
         )
-        dd_gph = {"gAperture": dd_gaperture, "gFind": dd_gfind}
-        np.save(outfile_app, dd_gph)
-        print("..done")
+        outfile_fin = outdir + fname_base + "_" + band.lower() + "_fin.npy"
+        outfile_app = outdir + fname_base + "_" + band.lower() + "_app.npy"
+
+        # Run or load gFind
+        if os.path.isfile(outfile_fin):
+            dd_gfind = np.load(outfile_fin, allow_pickle="TRUE").item()
+            t_bins = list(zip(dd_gfind[band]["t0"], dd_gfind[band]["t1"]))
+            print("Number of time bins:", len(t_bins))
+        else:
+            print("Running query with gFind for:\n", tt_src)
+            dd_gfind = gPhoton.gFind(
+                band=band, skypos=[tt_src["ra"][0], tt_src["dec"][0]]
+            )  # ,maxgap=100.,minexp=100.
+            t_bins = list(zip(dd_gfind[band]["t0"], dd_gfind[band]["t1"]))
+            np.save(outfile_fin, dd_gfind)
+
+        # Run or load gApperture
+        if os.path.isfile(outfile_app):
+            print("gAperture file already found, not running")
+        else:
+            print("Running lightcurve with gAperture..")
+            dd_gaperture = gPhoton.gAperture(
+                band=band,
+                skypos=[tt_src["ra"][0], tt_src["dec"][0]],
+                radius=ron,
+                annulus=[roff1, roff2],
+                tranges=t_bins,
+            )
+            dd_gph = {"gAperture": dd_gaperture, "gFind": dd_gfind}
+            np.save(outfile_app, dd_gph)
+            print("..done")
