47a233c6a440f78e056bef32eed978a2779517fd
Rolf Buehler <rolf.buehler@desy.de>
2023-10-04 10:39:50 +0200

    Added gPhoton light curve
    Former-commit-id: 7adb0c45193a505367105caf2a9f74982982fc96

--- vasca/source.py
+++ vasca/source.py
@@ -5,14 +5,17 @@ Created on Thu Jan 19 10:17:54 2023
 
 @author: buehler
 """
+import os
 import numpy as np
 from astropy import constants as cc
 from astropy import units as uu
+from astropy.table import Table, vstack
 from loguru import logger
 
 from vasca.tables import TableCollection
-from vasca.utils import query_vizier_sed, dd_id2filter,dd_filter2wavelength
-
+from vasca.utils import query_vizier_sed, dd_id2filter,dd_filter2id,dd_filter2wavelength, mag2flux, tgalex_to_astrotime
+from vasca.resource_manager import ResourceManager
+rm = ResourceManager()
 
 class Source(TableCollection):
     """
@@ -104,3 +107,66 @@ class Source(TableCollection):
 
         # Sort by wavelength
         self.tt_sed.sort("wavelength")
+
+    def add_gphoton_lc(self, s2n_min=3.):
+        """
+        Add light curve from gPhoton. Only include poitns with no flags.
+        Parameters
+        ----------
+        s2n_min: float, optional
+            Minimum significance of points for selection in light curve.
+        Returns
+        -------
+            None
+        """
+        def get_lc_from_gphoton_npfile(file_name, obs_filter):
+            "Helper function to load gphoton results pickel with numpy"
+            dd_gph = np.load(file_name, allow_pickle='TRUE').item()
+
+            # Get gphoton lc
+            keep_keys = ('t_mean', 'exptime', 'flux_bgsub', 'flux_bgsub_err', "flags",
+                         "mag_mcatbgsub", "mag_mcatbgsub_err_2","flags")
+            dd_gap = {x: dd_gph["gAperture"][x] for x in keep_keys if
+                      x in dd_gph["gAperture"]}
+            dd_gap["s2n"] = dd_gap['flux_bgsub'] / dd_gap['flux_bgsub_err']
+
+            #Rename key and change units
+            dd_gap["time_bin_size"] = dd_gap.pop('exptime')
+            # Units of flux_bgsub are in erg sec^-1 cm^-2 Å^-1. . Get also Jy flux from AB magnitude
+            dd_gap['flux'], dd_gap['flux_err'] = mag2flux(dd_gap["mag_mcatbgsub"],
+                                                          dd_gap["mag_mcatbgsub_err_2"])
+            # Units of time are in "GALEX Time" = "UNIX Time" - 315964800, change to MJD
+            dd_gap['time'] = tgalex_to_astrotime(dd_gap['t_mean'], "mjd")
+
+            dd_gap["obs_filter"] = [obs_filter] * len(dd_gap['flux'])
+            dd_gap["obs_filter_id"] = [dd_filter2id[obs_filter]] * len(dd_gap['flux'])
+            return Table(dd_gap)
+
+        # Get location of gphoton files
+        gphot_dir = rm.get_path("gal_gphoton", "sas_cloud")
+
+        #Prepare info for file reading
+        rg_src_id = self.tt_sources["rg_src_id"][0]
+        ra_src = round(self.tt_sources["ra"][0], 5)
+        dec_src = round(self.tt_sources["dec"][0], 5)
+
+        #Check if NUV file is present and load it, this is requires
+        fname_nuv = gphot_dir + "/gPhoton_ra" + str(ra_src) + "_dec" + str(dec_src) + "_nuv_app.npy"
+        if os.path.exists(fname_nuv):
+            tt_lc = get_lc_from_gphoton_npfile(fname_nuv,"NUV")
+        else:
+            logger.warning(f"gPhoton file not found {fname}")
+            return
+
+        #If FUV file present add it to table
+        fname_fuv = fname_nuv.replace("_nuv","_fuv")
+        if os.path.exists(fname_fuv):
+            tt_lc_fuv = get_lc_from_gphoton_npfile(fname_fuv, "FUV")
+            tt_lc = vstack([tt_lc, tt_lc_fuv])
+
+        #Add light curve table
+        self.add_table(tt_lc, "region:tt_gphoton_lc")
+
+        #Modify selection
+        sel = (self.tt_gphoton_lc["flags"]<0.5) * (self.tt_gphoton_lc["s2n"]>s2n_min)
+        self.tt_gphoton_lc["sel"] = sel
