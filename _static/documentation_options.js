7f6f5ac0daa161028d24f9b6fcc2eb25eea0a7cc
schliwiju <schliwiju@users.noreply.github.com>
2024-11-07 22:19:46 +0000

    deploy: 1373193835e0997f04819b73396ce65a16e0616b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.0.16.dev9+g90f10c0',
+    VERSION: '1.0.16.dev10+g1373193',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
---
+++ _static/tippy/tutorials/tutorial_var_stats.dc80c281-6841-4664-8270-353580de474b.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#synthetic-light-curves\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Synthetic Light Curves<a class=\"headerlink\" href=\"#synthetic-light-curves\" title=\"Link to this heading\">\u00b6</a></h2>", "a[href=\"../api/vasca/vasca.utils.html#vasca.utils.get_var_stat\"]": "<dt class=\"sig sig-object py\" id=\"vasca.utils.get_var_stat\">\n<span class=\"sig-prename descclassname\"><span class=\"pre\">vasca.utils.</span></span><span class=\"sig-name descname\"><span class=\"pre\">get_var_stat</span></span><span class=\"sig-paren\">(</span><em class=\"sig-param\"><span class=\"n\"><span class=\"pre\">vals</span></span></em>, <em class=\"sig-param\"><span class=\"n\"><span class=\"pre\">vals_err</span></span></em><span class=\"sig-paren\">)</span><a class=\"reference internal\" href=\"../_modules/vasca/utils.html#get_var_stat\"><span class=\"viewcode-link\"><span class=\"pre\">[source]</span></span></a></dt><dd><p>Calculate variability parameters</p></dd>", "a[href=\"#visualization\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Visualization<a class=\"headerlink\" href=\"#visualization\" title=\"Link to this heading\">\u00b6</a></h2>", "a[href=\"#variability-statistics\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">Variability Statistics<a class=\"headerlink\" href=\"#variability-statistics\" title=\"Link to this heading\">\u00b6</a></h1><p>This tutorial showcases VASCA\u2019s main statistics computation (<a class=\"reference internal\" href=\"../api/vasca/vasca.utils.html#vasca.utils.get_var_stat\" title=\"vasca.utils.get_var_stat\"><code class=\"xref myst py py-func docutils literal notranslate\"><span class=\"pre\">get_var_stat</span></code></a>) based\non synthetic data. The simple simulation framework allows for the variation of data\npoints, flux uncertainties, number of flare events as well as intrinsic variability\nparameters like amplitude and period.</p>"}
+skip_classes = ["headerlink", "sd-stretched-link"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(`div.content ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+                onShow(instance) {MathJax.typesetPromise([instance.popper]).then(() => {});},
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
